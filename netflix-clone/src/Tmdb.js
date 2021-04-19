31746b8857df7024247b19f39500a673f6d99811
Roymp3 <71888012+Roymp3@users.noreply.github.com>
2021-04-19 17:34:12 -0300

    arrumei

--- netflix-clone/src/Tmdb.js
+++ netflix-clone/src/Tmdb.js
@@ -18,7 +18,7 @@ export default{
 
                     slug: 'originals',
                     title: 'Originais do Netflix',
-                    items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api+key=${API_KEY}`)
+                    items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
                 },
                 {
                     slug: 'trending',
